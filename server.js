85179a5e2dd16ccf45dbf06cf7aa075ea37e8125
yonikosiner <yoni@kosiner.co.uk>
2020-11-26 14:01:58 +0000

    will it work

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/contact", (req, res) => {
+  res.send("Contact");
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.post("/contact", urlencodedParser, function (req, res) {
+  console.log(req.body.name);
+  console.log(req.body.email);
+  console.log(req.body.message);
 });
 
 app.listen(port, () => {
